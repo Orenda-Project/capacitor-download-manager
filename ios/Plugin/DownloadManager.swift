4c137847748d6ae67fbeba764c14899fc1f5838d
Nabi Ahmad <cs.nabiahmad@gmail.com>
2024-06-12 16:15:26 +0500

    URLSessionDownloadTask Implemented => Code Refactor

--- ios/Plugin/DownloadManager.swift
+++ ios/Plugin/DownloadManager.swift
@@ -2,23 +2,54 @@ import Foundation
 import Capacitor
 
 @objc public class DownloadManager: NSObject {
-    var delegate: DownloadDelegate?
+    public static let shared = DownloadManager()
+    var session: URLSession!
+    var activeDownloads: [URL: DownloadSession] = [:]
     var downloadList: [Download]?
-
+    var downloadDelegate : DownloadDelegate?
+    
+    
     override public init() {
         super.init()
+        let config = URLSessionConfiguration.background(withIdentifier: "pk.edu.niete.ios")
+        session = URLSession(configuration: config, delegate: self, delegateQueue: nil)
         loadDownloads()
+        
     }
-
+    func initDelegate(delegate: DownloadDelegate){
+        if downloadDelegate == nil {
+            self.downloadDelegate = delegate
+        }
+    }
+    
     func addDownload(download: Download) {
-        downloadList?.append(download)
+        if let (index,_) = getDownloadAt(url: download.url){
+            setDownloadAt(index: index , download: download)
+        }else{
+            downloadList?.append(download)
+            downloadDelegate?.onStatusChange(download, DownloadCallback.onAdded)
+        }
         saveDownloads()
     }
-    public func fetchDownloads() -> [Download]? {
-        return self.downloadList
+    
+    func getDownloadAt(url: String) -> (Int, Download)? {
+        if let index = downloadList?.firstIndex(where: { $0.url == url }){
+            return(index,downloadList?[index]) as? (Int, Download)
+        }else {
+            return nil
+        }
     }
-
-    // Function to save the downloads to UserDefaults
+    
+    func setDownload(download: Download) {
+        if let index = downloadList?.firstIndex(where: { $0.url == download.url }){
+            downloadList?[index] = download
+        }
+    }
+    
+    func setDownloadAt(index: Int, download: Download) {
+        downloadList?[index] = download
+    }
+    
     func saveDownloads() {
         let encoder = JSONEncoder()
         if let encoded = try? encoder.encode(self.downloadList) {
@@ -26,8 +57,8 @@ import Capacitor
             defaults.set(encoded, forKey: "Downloads")
         }
     }
-    // Function to load the downloads from UserDefaults
-    private func loadDownloads() {
+    
+    func loadDownloads() {
         let defaults = UserDefaults.standard
         if let savedDownloads = defaults.object(forKey: "Downloads") as? Data {
             let decoder = JSONDecoder()
@@ -36,9 +67,10 @@ import Capacitor
             }
         } else {
             downloadList = [Download]()
+            activeDownloads = [:]
         }
     }
-
+    
     func deleteDownloads(by downloadsToRemove: [String]) -> [Download]? {
         guard var downloads = downloadList else { return nil }
         var removedDownloads: [Download] = []
@@ -57,9 +89,7 @@ import Capacitor
         return removedDownloads.isEmpty ? nil : removedDownloads
     }
     
-    public typealias ProgressEmitter = (_ bytes: Int64, _ contentLength: Int64) -> Void
-
-    public func getDirectory(directory: String?) -> FileManager.SearchPathDirectory? {
+    func getDirectory(directory: String?) -> FileManager.SearchPathDirectory? {
         if let directory = directory {
             switch directory {
             case "CACHE":
@@ -72,189 +102,172 @@ import Capacitor
         }
         return nil
     }
-
-    @objc public func downloadFile(call: CAPPluginCall, url: URL, emitter: @escaping ProgressEmitter, config: InstanceConfiguration?) throws {
-        loadDownloads()
-        addDownload(download: Download(file: url.absoluteString, url: url.absoluteString))
-
-        let directory = "LIBRARY"
-        let path = url.lastPathComponent
-        var urlString = url.absoluteString
-
-        func handleDownload(downloadLocation: URL?, response: URLResponse?, error: Error?) {
-            if let error = error {
-                CAPLog.print("Error on download file", String(describing: downloadLocation), String(describing: response), String(describing: error))
-                call.reject(error.localizedDescription, "DOWNLOAD", error, nil)
-                return
+    
+    func getFileUrl(at path: String, in directory: String?) -> URL? {
+        if let directory = getDirectory(directory: directory) {
+            guard let dir = FileManager.default.urls(for: directory, in: .userDomainMask).first else {
+                return nil
             }
-
-            if let httpResponse = response as? HTTPURLResponse {
-                if !(200...299).contains(httpResponse.statusCode) {
-                    CAPLog.print("Error downloading file:", urlString, httpResponse)
-                    call.reject("Error downloading file: \(urlString)", "DOWNLOAD")
-                    return
-                }
-                HttpRequestHandler.setCookiesFromResponse(httpResponse, config)
+            if !path.isEmpty {
+                return dir.appendingPathComponent(path)
             }
-
-            guard let location = downloadLocation else {
-                call.reject("Unable to get file after downloading")
-                return
-            }
-
-            let fileManager = FileManager.default
-
-            if let foundDir = getDirectory(directory: directory) {
-                let dir = fileManager.urls(for: foundDir, in: .userDomainMask).first
-
-                do {
-                    let dest = dir!.appendingPathComponent(path)
-                    CAPLog.print("Attempting to write to file destination: \(dest.absoluteString)")
-
-                    if !FileManager.default.fileExists(atPath: dest.deletingLastPathComponent().absoluteString) {
-                        try FileManager.default.createDirectory(at: dest.deletingLastPathComponent(), withIntermediateDirectories: true, attributes: nil)
-                    }
-
-                    if FileManager.default.fileExists(atPath: dest.relativePath) {
-                        do {
-                            CAPLog.print("File already exists. Attempting to remove file before writing.")
-                            try fileManager.removeItem(at: dest)
-                        } catch let error {
-                            call.reject("Unable to remove existing file: \(error.localizedDescription)")
-                            return
-                        }
-                    }
-
-                    try fileManager.moveItem(at: location, to: dest)
-                    CAPLog.print("Downloaded file successfully! \(dest.absoluteString)")
-
-                    if let index = self.downloadList?.firstIndex(where: { $0.url == downloadLocation?.absoluteString }) {
-                        if var newDownload = self.downloadList?[index] {
-                            newDownload.status = "COMPLETED"
-                            newDownload.fileUri = dest.absoluteString
-                            newDownload.error = "none"
-                            self.downloadList?[index] = newDownload
-                            self.saveDownloads()
-                        }
-                    }
-
-                    call.resolve(["path": dest.absoluteString])
-                } catch let error {
-                    call.reject("Unable to download file: \(error.localizedDescription)", "ERROR", error)
-                    return
-                }
-            } else {
-                call.reject("Unable to download file. Couldn't find directory \(directory)")
-            }
+            return dir
+        } else {
+            return URL(string: path)
         }
-
-        let method = call.getString("method", "GET")
-        let headers = (call.getObject("headers") ?? [:]) as [String: Any]
-        let params = (call.getObject("params") ?? [:]) as [String: Any]
-        let connectTimeout = call.getDouble("connectTimeout")
-        let readTimeout = call.getDouble("readTimeout")
-
-        if urlString == urlString.removingPercentEncoding {
-            guard let encodedUrlString = urlString.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)  else { throw URLError(.badURL) }
-            urlString = encodedUrlString
-        }
-
-        let request = try HttpRequestHandler.CapacitorHttpRequestBuilder()
-            .setUrl(urlString)
-            .setMethod(method)
-            .setUrlParams(params)
-            .openConnection()
-            .build()
-
-        request.setRequestHeaders(headers)
-
-        // Timeouts in iOS are in seconds. So read the value in millis and divide by 1000
-        let timeout = (connectTimeout ?? readTimeout ?? 600000.0) / 1000.0
-        request.setTimeout(timeout)
-
-        if let data = call.options["data"] as? JSValue {
-            do {
-                try request.setRequestBody(data)
-            } catch {
-                // Explicitly reject if the http request body was not set successfully,
-                // so as to not send a known malformed request, and to provide the developer with additional context.
-                call.reject(error.localizedDescription, (error as NSError).domain, error, nil)
-                return
-            }
-        }
-
-        var session: URLSession!
-        var task: URLSessionDownloadTask!
-        let urlRequest = request.getUrlRequest()
-
-        class ProgressDelegate: NSObject, URLSessionDataDelegate, URLSessionDownloadDelegate {
-            private var handler: (URL?, URLResponse?, Error?) -> Void
-            private var downloadLocation: URL?
-            private var emitter: (Int64, Int64) -> Void
-
-            init(downloadHandler: @escaping (URL?, URLResponse?, Error?) -> Void, progressEmitter: @escaping (Int64, Int64) -> Void) {
-                handler = downloadHandler
-                emitter = progressEmitter
-            }
-
-            func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didWriteData bytesWritten: Int64, totalBytesWritten: Int64, totalBytesExpectedToWrite: Int64) {
-                if totalBytesExpectedToWrite > 0 {
-                    emitter(totalBytesWritten, totalBytesExpectedToWrite)
-                }
-            }
-
-            func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didFinishDownloadingTo location: URL) {
-                downloadLocation = location
-                handler(downloadLocation, downloadTask.response, downloadTask.error)
-            }
-
-            func urlSession(_ session: URLSession, task: URLSessionTask, didCompleteWithError error: Error?) {
-                if error != nil {
-                    handler(downloadLocation, task.response, error)
-                }
-            }
-        }
-
-        let progressDelegate = ProgressDelegate(downloadHandler: handleDownload, progressEmitter: emitter)
-        session = URLSession(configuration: .default, delegate: progressDelegate, delegateQueue: nil)
-        task = session.downloadTask(with: urlRequest)
-        task.resume()
     }
-
     
-        /**
-        * Get the URL for this file, supporting file:// paths and
-        * files with directory mappings.
-        */
-    @objc public func getFileUrl(at path: String, in directory: String?) -> URL? {
-           if let directory = getDirectory(directory: directory) {
-               guard let dir = FileManager.default.urls(for: directory, in: .userDomainMask).first else {
-                   return nil
-               }
-               if !path.isEmpty {
-                   return dir.appendingPathComponent(path)
-               }
-               return dir
-           } else {
-               return URL(string: path)
-           }
-       }
-    
-    @objc func deleteFile(path:String) {
-         if let range = path.range(of: "/", options: .backwards) {
+    func deleteFile(path:String) {
+        if let range = path.range(of: "/", options: .backwards) {
             let fileName = String(path[range.upperBound...])
-            var directory = "LIBRARY"
+            let directory = "LIBRARY"
             guard let fileUrl = getFileUrl(at: fileName, in: directory) else {
                 CAPLog.print("Invalid path")
                 return
             }
-
+            
             do {
                 if FileManager.default.fileExists(atPath: fileUrl.path) {
                     try FileManager.default.removeItem(atPath: fileUrl.path)
                 }
             } catch let error as NSError {
                 CAPLog.print(error.localizedDescription, error)
+            }
+        }
+    }
+    
+    func startDownload(from url: URL) {
+        let download = DownloadSession(url: url)
+        activeDownloads[url] = download
+        addDownload(download: Download(file: url.lastPathComponent, fileUri: url.relativePath, url: url.absoluteString, status: DownloadStatus.ADDED, downloaded: 0, total: 0))
+        download.task = session.downloadTask(with: url)
+        download.task?.resume()
+    }
+    
+    func startDownloadSession(from url: URL, task: URLSessionDownloadTask ) {
+        let download = DownloadSession(url: url)
+        activeDownloads[url] = download
+        download.task = task
+        download.task?.resume()
+    }
+    
+    func resumeDownload(from url: URL, resumeData: Data) {
+        let download = DownloadSession(url: url)
+        activeDownloads[url] = download
+        download.task = session.downloadTask(withResumeData: resumeData)
+        download.task?.resume()
+    }
+    
+    func saveResumeData(for url: URL) {
+        if let download = activeDownloads[url] {
+            download.task?.cancel(byProducingResumeData: { resumeData in
+                download.resumeData = resumeData
+                UserDefaults.standard.set(resumeData, forKey: url.absoluteString)
+            })
+        }
+    }
+    
+    public func fetchDownloads() -> [Download]? {
+        return self.downloadList
+    }
+    
+    public func saveActives(){
+        for download in activeDownloads.values {
+            saveResumeData(for: download.url)
+        }
+    }
+    
+    public func resumeActives(){
+        let defaults = UserDefaults.standard
+        downloadList?.forEach({ download in
+            if(download.status == DownloadStatus.COMPLETED){
+                return
+            }else if let resumeData = defaults.data(forKey: download.url) {
+                resumeDownload(from: URL(string: download.url)!, resumeData: resumeData)
+                defaults.removeObject(forKey: download.url)
+            } else {
+                startDownload(from: URL(string: download.url)!)
+            }
+        })
+    }
+    
+}
+
+extension DownloadManager: URLSessionDownloadDelegate {
+    
+    public func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didFinishDownloadingTo location: URL) {
+        // Handle file move or process after download completion
+        // Define the FileManager
+        let fileManager = FileManager.default
+        
+        // Get the .library directory URL
+        guard let libraryDirectory = fileManager.urls(for: .libraryDirectory, in: .userDomainMask).first else {
+            print("Could not find the library directory")
+            return
+        }
+        
+        // Create a destination URL in the .library directory
+        let destinationURL = libraryDirectory.appendingPathComponent(downloadTask.originalRequest?.url?.lastPathComponent ?? "file")
+        
+        // Remove any existing file at the destination URL
+        if fileManager.fileExists(atPath: destinationURL.path) {
+            do {
+                try fileManager.removeItem(at: destinationURL)
+            } catch {
+                print("Error removing existing file: \(error.localizedDescription)")
+                return
+            }
+        }
+        
+        // Move the downloaded file to the destination URL
+        do {
+            try fileManager.moveItem(at: location, to: destinationURL)
+            print("File moved to: \(destinationURL.path)")
+            
+            guard let url = downloadTask.originalRequest?.url?.absoluteString else {return}
+            if var (index, download) = getDownloadAt(url: url) {
+                download.file = destinationURL.path
+                download.status = DownloadStatus.COMPLETED
+                downloadList?[index] = download
+                downloadDelegate?.onStatusChange(download,DownloadCallback.onCompleted)
+                saveDownloads()
+            }
+            
+        } catch {
+            print("Error moving file: \(error.localizedDescription)")
+        }
+    }
+    
+    public func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didWriteData bytesWritten: Int64, totalBytesWritten: Int64, totalBytesExpectedToWrite: Int64) {
+        guard let url = downloadTask.originalRequest?.url?.absoluteString else {return}
+        if var (index, download) = getDownloadAt(url: url) {
+            let progress = (totalBytesWritten/totalBytesExpectedToWrite)*100
+            download.downloaded = Int(totalBytesWritten)
+            download.total = Int(totalBytesExpectedToWrite)
+            download.status = progress == 100 ? DownloadStatus.COMPLETED : DownloadStatus.DOWNLOADING
+            downloadList?[index] = download
+            downloadDelegate?.onStatusChange(download, DownloadCallback.onProgress)
+            saveDownloads()
+        }
+        
+    }
+    
+    public func urlSession(_ session: URLSession, task: URLSessionTask, didCompleteWithError error: Error?) {
+        guard let url = task.originalRequest?.url else { return }
+        activeDownloads[url] = nil
+        if let error = error as NSError?, let resumeData = error.userInfo[NSURLSessionDownloadTaskResumeData] as? Data {
+            UserDefaults.standard.set(resumeData, forKey: url.absoluteString)
+        }
+        else if let error = error as NSError?, error.domain == NSURLErrorDomain {
+            if let resumeData = error.userInfo[NSURLSessionDownloadTaskResumeData] as? Data {
+                UserDefaults.standard.set(resumeData, forKey: url.absoluteString)
+                if var (index, download) = getDownloadAt(url: url.absoluteString) {
+                    download.status = DownloadStatus.FAILED
+                    download.error = error.localizedDescription
+                    downloadList?[index] = download
+                    downloadDelegate?.onStatusChange(download, DownloadCallback.onError)
+                    saveDownloads()
+                }
             }
         }
     }
