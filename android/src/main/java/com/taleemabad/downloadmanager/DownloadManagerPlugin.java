d34ed72678ea68161fbb3c2026f8582652da2eb4
Abdurrehman Afridi <abdurrehman.afridi.5@gmail.com>
2024-08-29 11:36:24 +0500

    refac: docstrings and code cleanup

--- android/src/main/java/com/taleemabad/downloadmanager/DownloadManagerPlugin.java
+++ android/src/main/java/com/taleemabad/downloadmanager/DownloadManagerPlugin.java
@@ -18,6 +18,7 @@ import com.getcapacitor.JSObject;
 import com.getcapacitor.Plugin;
 import com.getcapacitor.PluginCall;
 import com.getcapacitor.PluginMethod;
+import com.getcapacitor.annotation.ActivityCallback;
 import com.getcapacitor.annotation.CapacitorPlugin;
 import com.google.gson.Gson;
 import com.google.mlkit.vision.documentscanner.GmsDocumentScannerOptions;
@@ -29,7 +30,6 @@ import com.tonyodev.fetch2.Error;
 import com.tonyodev.fetch2.FetchListener;
 import com.tonyodev.fetch2core.DownloadBlock;
 
-import org.json.JSONArray;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -48,28 +48,27 @@ public class DownloadManagerPlugin extends Plugin implements FetchListener {
         scannerLauncher = activity.registerForActivityResult(new ActivityResultContracts.StartIntentSenderForResult(), this::handleScanResult);
     }
 
+    @PluginMethod
+    public void startScan(PluginCall call) {
+        String mode = call.getString("mode", "FULL");
+        initScanner(call, mode);
+    }
+
     private void initDownloadManager() {
         if (downloadManager == null)
             downloadManager = DownloadManager.getInstance(this.getActivity(), this);
     }
 
 
+    /**
+     * Initialize the document scanner and configure its settings.
+     *
+     * @param call contains JS inputs and lets you return results
+     * @param mode scanning mode
+     */
     private void initScanner(PluginCall call, String mode) {
-        boolean enableGalleryImport = Boolean.TRUE.equals(call.getBoolean("enableGalleryImport", true));
-        Integer pageLimit = call.getInt("pageLimit", 0);
-        String outputFormats = call.getString("outputFormats", "BOTH");
-
         GmsDocumentScannerOptions.Builder scannerOptions = new GmsDocumentScannerOptions.Builder();
-
-        setOutputFormats(scannerOptions, outputFormats != null ? outputFormats : "BOTH");
-
-        scannerOptions
-                .setGalleryImportAllowed(enableGalleryImport)
-                .setScannerMode(getScannerMode(mode));
-
-        if (pageLimit != null && pageLimit > 0) {
-            scannerOptions.setPageLimit(pageLimit);
-        }
+        configureScannerOptions(call, scannerOptions, mode);
 
         GmsDocumentScanning.getClient(scannerOptions.build())
                 .getStartScanIntent(getActivity())
@@ -83,10 +82,59 @@ public class DownloadManagerPlugin extends Plugin implements FetchListener {
         saveCall(call);
     }
 
-    @PluginMethod
-    public void startScan(PluginCall call) {
-        String mode = call.getString("mode", "FULL");
-        initScanner(call, mode);
+    /**
+     * Configure the document scanner options based on the parameters from the JS call.
+     *
+     * @param call           JS call with the configuration options
+     * @param scannerOptions Scanner options builder
+     * @param mode           Scanning mode
+     */
+    private void configureScannerOptions(PluginCall call, GmsDocumentScannerOptions.Builder scannerOptions, String mode) {
+        boolean enableGalleryImport = Boolean.TRUE.equals(call.getBoolean("enableGalleryImport", true));
+        Integer pageLimit = call.getInt("pageLimit", 0);
+        String outputFormats = call.getString("outputFormats", "BOTH");
+
+        setOutputFormats(scannerOptions, outputFormats != null ? outputFormats : "BOTH");
+        scannerOptions
+                .setGalleryImportAllowed(enableGalleryImport)
+                .setScannerMode(getScannerMode(mode));
+
+        if (pageLimit != null && pageLimit > 0) {
+            scannerOptions.setPageLimit(pageLimit);
+        }
+    }
+
+    /**
+     * Handle the result from the document scanner activity.
+     *
+     * @param result the result from the document scanner activity
+     */
+    @ActivityCallback
+    private void handleScanResult(ActivityResult result) {
+        PluginCall savedCall = getSavedCall();
+        if (savedCall == null) return;
+
+        JSObject response = new JSObject();
+
+        if (result.getResultCode() == Activity.RESULT_OK) {
+            GmsDocumentScanningResult scanResult = GmsDocumentScanningResult.fromActivityResultIntent(result.getData());
+            List<String> imageUris = new ArrayList<>();
+
+            if (scanResult != null && scanResult.getPages() != null) {
+                for (GmsDocumentScanningResult.Page page : scanResult.getPages()) {
+                    imageUris.add(page.getImageUri().toString());
+                }
+            }
+
+            String pdfUri = scanResult != null && scanResult.getPdf() != null ? scanResult.getPdf().getUri().toString() : null;
+
+            response.put("images", new JSArray(imageUris));
+            response.put("pdf", pdfUri);
+            savedCall.resolve(response);
+        } else {
+            response.put("status", "cancel");
+            savedCall.reject("Scanning was cancelled or failed with unexpected error.");
+        }
     }
 
     private void setOutputFormats(GmsDocumentScannerOptions.Builder scannerOptions, String outputFormats) {
@@ -100,48 +148,24 @@ public class DownloadManagerPlugin extends Plugin implements FetchListener {
         }
     }
 
+    /**
+     * Get the scanner mode based on the provided mode string.
+     *
+     * @param mode Scanner mode string
+     * @return Scanner mode constant
+     */
     private int getScannerMode(String mode) {
-        switch (mode) {
-            case "FULL" -> {
-                return GmsDocumentScannerOptions.SCANNER_MODE_FULL;
-            }
-            case "BASE" -> {
-                return GmsDocumentScannerOptions.SCANNER_MODE_BASE;
-            }
-            case "BASE_WITH_FILTER" -> {
-                return GmsDocumentScannerOptions.SCANNER_MODE_BASE_WITH_FILTER;
-            }
+        return switch (mode) {
+            case "FULL" -> GmsDocumentScannerOptions.SCANNER_MODE_FULL;
+            case "BASE" -> GmsDocumentScannerOptions.SCANNER_MODE_BASE;
+            case "BASE_WITH_FILTER" -> GmsDocumentScannerOptions.SCANNER_MODE_BASE_WITH_FILTER;
             default -> {
                 Log.e(TAG, "Unknown scanning mode: " + mode);
-                return -1;
+                yield -1;
             }
-        }
+        };
     }
 
-    private void handleScanResult(ActivityResult result) {
-        PluginCall savedCall = getSavedCall();
-        if (savedCall == null) return;
-
-        if (result.getResultCode() == Activity.RESULT_OK) {
-            GmsDocumentScanningResult scanResult = GmsDocumentScanningResult.fromActivityResultIntent(result.getData());
-            List<String> imageUris = new ArrayList<>();
-            assert scanResult != null;
-            if (scanResult.getPages() != null) {
-                for (GmsDocumentScanningResult.Page page : scanResult.getPages()) {
-                    imageUris.add(page.getImageUri().toString());
-                }
-            }
-
-            String pdfUri = scanResult.getPdf() != null ? scanResult.getPdf().getUri().toString() : null;
-
-            JSObject ret = new JSObject();
-            ret.put("images", new JSONArray(imageUris));
-            ret.put("pdf", pdfUri);
-            savedCall.resolve(ret);
-        } else {
-            savedCall.reject("Scanning was cancelled or failed with unexpected error.");
-        }
-    }
 
     @RequiresApi(api = Build.VERSION_CODES.R)
     @PluginMethod
